bbe6b787b6607b299bb7906848588c06bb4032fb
Ajay <iamask@live.com>
2024-01-01 20:29:07 +0530

    updated access + zt

--- zero_trust/access/access.tf
+++ zero_trust/access/access.tf
@@ -1,12 +1,12 @@
 resource "cloudflare_access_application" "staging_app" {
   zone_id          = "bcbaeaa288da7324b61d91b0e41adc90"
   name             = "staging application"
-  domain           = "staging.example.com"
+  domain           = "staging.zxc.co.in"
   type             = "self_hosted"
   session_duration = "24h"
   cors_headers {
     allowed_methods   = ["GET", "POST", "OPTIONS"]
-    allowed_origins   = ["https://example.com"]
+    allowed_origins   = ["https://zxc.co.in"]
     allow_credentials = true
     max_age           = 20
   }
