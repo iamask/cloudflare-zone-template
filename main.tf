187e97a0141aca6148fd797f5e7b4cd5083d4d80
Ajay <iamask@live.com>
2024-01-01 17:25:41 +0530

    update waf 2 with fmt

--- main.tf
+++ main.tf
@@ -60,23 +60,23 @@ resource "cloudflare_ruleset" "zone_level_managed_waf" {
   rules {
     action = "execute"
     action_parameters {
-      id = "efb7b8c949ac4650a09736fc376e9aee"
+      id      = "efb7b8c949ac4650a09736fc376e9aee"
       version = "latest"
     }
-    expression = "true"
+    expression  = "true"
     description = "Execute Cloudflare Managed Ruleset on my zone-level phase entry point ruleset"
-    enabled = true
+    enabled     = true
   }
 
   # Execute Cloudflare OWASP Core Ruleset
   rules {
     action = "execute"
     action_parameters {
-      id = "4814384a9e5d4991b9815dcfc25d2f1f"
+      id      = "4814384a9e5d4991b9815dcfc25d2f1f"
       version = "latest"
     }
-    expression = "true"
+    expression  = "true"
     description = "Execute Cloudflare OWASP Core Ruleset on my zone-level phase entry point ruleset"
-    enabled = true
+    enabled     = true
   }
 }
