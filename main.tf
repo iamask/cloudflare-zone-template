fcb44bd3ad2dda8783aee2921646c1df534b0c20
Ajay <iamask@live.com>
2024-01-01 14:54:08 +0530

    initial DNS

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+      
+    }
+    
+  }
+}
+
+terraform {
+  cloud {
+    organization = "iamask"
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+}
+
+
+provider "cloudflare" {
+  api_token = var.TF_VAR_CLOUDFLARE_API_TOKEN 
+}
+
+resource "cloudflare_record" "www" {
+  zone_id = "bcbaeaa288da7324b61d91b0e41adc90"
+  name    = "www"
+  value   = "203.0.113.10"
+  type    = "A"
+  proxied = true
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+# variables.tf
+
+variable "TF_VAR_CLOUDFLARE_API_TOKEN" {
+  description = "Cloudflare API token"
+}
