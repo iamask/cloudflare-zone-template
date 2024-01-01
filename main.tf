4cbbda0a57fcdab3225b95ac59f26fb070546775
Ajay <iamask@live.com>
2024-01-01 15:51:48 +0530

    added new a record

--- main.tf
+++ main.tf
@@ -30,3 +30,11 @@ resource "cloudflare_record" "www" {
   type    = "A"
   proxied = true
 }
+
+resource "cloudflare_record" "a" {
+  zone_id = "bcbaeaa288da7324b61d91b0e41adc90"
+  name    = "aa"
+  value   = "1.0.23.100"
+  type    = "A"
+  proxied = true
+}
