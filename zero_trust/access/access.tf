7aea9b14fac3e1181b2c4ccc4e6720859b6b5b00
Ajay <iamask@live.com>
2024-01-01 20:32:52 +0530

    updated access to tf.zxc.co.in domain/zone

--- zero_trust/access/access.tf
+++ zero_trust/access/access.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_access_application" "staging_app" {
   zone_id          = "bcbaeaa288da7324b61d91b0e41adc90"
   name             = "staging application"
-  domain           = "staging.zxc.co.in"
+  domain           = "staging.tf.zxc.co.in"
   type             = "self_hosted"
   session_duration = "24h"
   cors_headers {
