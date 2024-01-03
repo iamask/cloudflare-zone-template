723a1b9586a00cd67dd70410ec4236783ec68106
Ajay <iamask@live.com>
2024-01-03 22:43:23 +0530

    added access appplication

---
+++ security/zero_trust/access/access.tf
@@ -0,0 +1,13 @@
+resource "cloudflare_access_application" "staging_app" {
+  zone_id          = "bcbaeaa288da7324b61d91b0e41adc90"
+  name             = "staging application"
+  domain           = "staging.tf.zxc.co.in"
+  type             = "self_hosted"
+  session_duration = "24h"
+  cors_headers {
+    allowed_methods   = ["GET", "POST", "OPTIONS"]
+    allowed_origins   = ["https://zxc.co.in"]
+    allow_credentials = true
+    max_age           = 20
+  }
+}
