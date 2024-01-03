bb80ee7b5380325de800c0ad31c8af96520995d1
Ajay <iamask@live.com>
2024-01-03 21:54:16 +0530

    reverted

--- main.tf
+++ main.tf
@@ -27,30 +27,6 @@ module "dns" {
 }
 
 
-module "managed_rules" {
-  source = "./security"
-}
-
-module "custom_rules" {
-  source = "./security"
-}
-
-module "rate_limit" {
-  source = "./security"
-}
-
-module "cache_rules" {
-  source = "./cache_rules"
-}
-
-module "transform_rules" {
-  source = "./transform_rules"
-}
-
-module "zerotrust_access" {
-  source = "./zero_trust/access"
-}
-
 resource "cloudflare_zone_settings_override" "example-com-settings" {
   zone_id = "bcbaeaa288da7324b61d91b0e41adc90"
 
@@ -59,4 +35,25 @@ resource "cloudflare_zone_settings_override" "example-com-settings" {
     automatic_https_rewrites = "on"
     ssl                      = "strict"
   }
+}
+
+module "managed_rules" {
+  source = "./security/managed_rules"
+}
+
+module "custom_rules" {
+  source = "./security/custom_rules"
+}
+
+module "cache_rules" {
+  source = "./cache_rules"
+}
+
+
+module "transform_rules" {
+  source = "./zero_trust/access"
+}
+
+module "zerotrust_access" {
+  source = "./zero_trust/access"
 }
