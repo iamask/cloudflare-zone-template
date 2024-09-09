e9a65c10d7ac2bcb708f0d24d32d6c51625b270b
Ajay <iamask@live.com>
2024-09-09 22:15:40 +0530

    Update custom_hostname.tf

--- TLS/custom_hostnames/custom_hostname.tf
+++ TLS/custom_hostnames/custom_hostname.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_custom_hostname" "example" {
   zone_id  = var.ZONE_ID
-  hostname = "checkout.netflix.com"
+  hostname = "checkout.netflix.co.in"
   ssl {
     method = "HTTP"
   }
