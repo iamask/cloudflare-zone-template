b77a8cd2260e4747f02189b7c40f4e491c2e8c49
Ajay <iamask@live.com>
2024-09-09 23:36:22 +0530

    Update custom_hostnames.tf

--- tls/custom_hostnames/custom_hostnames.tf
+++ tls/custom_hostnames/custom_hostnames.tf
@@ -1,7 +1,27 @@
-resource "cloudflare_custom_hostname" "custom_hostnames" {
+resource "cloudflare_custom_hostname" "merchant1" {
   zone_id  = var.ZONE_ID
   hostname = "checkout.netflix.com"
   ssl {
     method = "http"
   }
+   custom_metadata = {
+    "customer" = "premium"
+    "security_level"   = "low"
+    "customer_id" = 123
+  }
 }
+
+
+resource "cloudflare_custom_hostname" "merchant2" {
+  zone_id  = var.ZONE_ID
+  hostname = "spotify.tf.zxc.co.in"
+  ssl {
+    method = "txt"
+  }
+   custom_metadata = {
+    "customer" = "free"
+    "security_level"   = "low"
+    "customer_id" = 124
+  }
+}
+
